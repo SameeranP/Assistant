fa7952a00cb2f6b31343f81139fc2bc72c68cbcb
SameeranP <51751179+SameeranP@users.noreply.github.com>
2020-08-01 12:58:13 +0530

    Update and rename scrapper.py to Price_alert.py
    Send me a mail if the price of an item reduces or breaks a certain threshold on Amazon.

--- Price_alert.py
+++ Price_alert.py
@@ -16,12 +16,12 @@ def send_mail():
     msg = f"Subject : {subject} \n \n {body}"
     
 
-    server.sendmail('sameeranpandey@gmail.com', 'sameeran.sameeran18@vit.edu', msg)
+    server.sendmail('Your_Email_Id@gmail.com', 'Secondary_email@gmail.com', msg)
     print("email sent")
     server.quit()
 
 
-URL = "https://www.amazon.in/Pantene-Silky-Smooth-Care-Shampoo/dp/B0764M9QVT/ref=sr_1_7?keywords=pantene&qid=1566390239&s=gateway&sr=8-7"
+URL = "https://www.amazon.in/Pantene-Silky-Smooth-Care-Shampoo/dp/B0764M9QVT/ref=sr_1_7?keywords=pantene&qid=1566390239&s=gateway&sr=8-7" //URL of the price section of product
 
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'}
 
